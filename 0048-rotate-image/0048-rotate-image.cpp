430c367c15880bf2430069dabca6c88346a3fe67
avinashtorm <avinashchandel223@gmail.com>
2026-08-04 00:34:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (38.02%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& a) {
+        int n=a.size(),x;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(a[i][j],a[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                swap(a[i][j],a[i][n-j-1]);
+            }
+        }
+        }
+};
