80de783388894c298b20732703cd8d34fc10e775
avinashtorm <avinashchandel223@gmail.com>
2026-08-04 09:39:32 +0530

    Time: 0 ms (100%), Space: 13.7 MB (62.71%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans;
+        for(auto i:s){
+            if(!ans.empty()&&ans.back()==i){
+                ans.pop_back();
+            }else{
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
