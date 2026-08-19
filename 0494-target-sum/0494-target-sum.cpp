187e8bb7c0682a2c98300ae36e61001d88d9224d
avinashtorm <avinashchandel223@gmail.com>
2026-08-19 15:36:47 +0530

    Time: 3 ms (85.96%), Space: 11.8 MB (75.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-int f(vector<int>&a,int n,int s){
-    if(n==0&&s==0)return 1;
-    if(n==0)return 0;
-    return f(a,n-1,s-a[n-1])+f(a,n-1,s+a[n-1]);
-}
-    int findTargetSumWays(vector<int>& a, int s) {
-        return f(a,a.size(),s);
+  int t[21][2001];
+  int f(vector<int>&a,int n,int s1,int s,int d){
+      if(n==0&&s1==d)return 1;
+      if(n==0)return 0;
+      if(t[n][s1+1000]!=-1)return t[n][s1+1000];
+      return t[n][s1+1000]=f(a,n-1,s1+a[n-1],s,d)+f(a,n-1,s1-a[n-1],s,d);
+  }
+    int findTargetSumWays(vector<int>& a, int d) {
+        memset(t,-1,sizeof(t));
+        int s=accumulate(a.begin(),a.end(),0);
+        int n=a.size();
+        return f(a,n,0,s,d);
     }
 };
