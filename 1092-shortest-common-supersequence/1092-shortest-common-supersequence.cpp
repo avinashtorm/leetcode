f35e1cbabbb012e978ade5789fd12781a256f476
avinashtorm <avinashchandel223@gmail.com>
2026-08-21 15:20:48 +0530

    Time: 27 ms (36.33%), Space: 27 MB (11.34%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,60 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> t;
-    string s = "";
+    vector<vector<int>>t;
+    string s="";
 
-    string shortestCommonSupersequence(string &s1, string &s2) {
+    string shortestCommonSupersequence(string &s1,string &s2){
+        int n=s1.size(),m=s2.size();
+        t.assign(n+1,vector<int>(m+1,0));
 
-        int n = s1.size();
-        int m = s2.size();
-
-        t.assign(n+1, vector<int>(m+1, 0));
-
-        // LCS table
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-
-                if(s1[i-1] == s2[j-1])
-                    t[i][j] = 1 + t[i-1][j-1];
-
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1])
+                    t[i][j]=1+t[i-1][j-1];
                 else
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
             }
         }
 
-        // Same as f(s1,s2,n,m)
-        while(n != 0 || m != 0) {
-
-            if(n == 0) {
-                s += s2[m-1];
-                m--;
-            }
-
-            else if(m == 0) {
-                s += s1[n-1];
-                n--;
-            }
-
-            else if(s1[n-1] == s2[m-1]) {
-                s += s1[n-1];
+        while(n>0&&m>0){
+            if(s1[n-1]==s2[m-1]){
+                s.push_back(s1[n-1]);
                 n--;
                 m--;
             }
-
-            else if(t[n-1][m] >= t[n][m-1]) {
-                s += s1[n-1];
+            else if(t[n-1][m]>t[n][m-1]){
+                s.push_back(s1[n-1]);
                 n--;
             }
-
-            else {
-                s += s2[m-1];
+            else{
+                s.push_back(s2[m-1]);
                 m--;
             }
         }
 
-        reverse(s.begin(), s.end());
+        while(n>0){
+            s.push_back(s1[n-1]);
+            n--;
+        }
 
+        while(m>0){
+            s.push_back(s2[m-1]);
+            m--;
+        }
+
+        reverse(s.begin(),s.end());
         return s;
     }
 };
-
