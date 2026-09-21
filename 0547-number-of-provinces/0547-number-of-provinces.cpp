f25169c2b3d0fc4b82fb2ebcdf6f86770d289e8e
avinashtorm <avinashchandel223@gmail.com>
2026-09-21 18:18:51 +0530

    Time: 0 ms (100%), Space: 20.4 MB (36.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-void dfs(int c,int n,vector<bool>&visited,vector<vector<int>>&a){
-    visited[c]=true;
-    for(int i=0;i<n;i++){
-        if(a[c][i]==0)continue;
-        if(visited[i]==true)continue;
-        dfs(i,n,visited,a);
-    }
-}
-    int findCircleNum(vector<vector<int>>&a) {
-        int n=a.size();
-        vector<bool>visited(n,false);
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]==true)continue;
-            dfs(i,n,visited,a);
-            count++;
+    int findCircleNum(vector<vector<int>>&b) {
+       int n=b.size(),count=0;
+       vector<int>v(n,0);
+       vector<vector<int>>a(n);
+       queue<int>q;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(b[i][j]==1&&i!=j){
+                a[i].push_back(j);
+            }
         }
-        return count;
+       }
+       for(int i=0;i<n;i++){
+        if(v[i]==1)continue;
+        count++;
+        v[i]=1;
+        q.push(i);
+        while(!q.empty()){
+            int k=q.front();
+            q.pop();
+            for(int j=0;j<a[k].size();j++){
+                if(v[a[k][j]]==1)continue;
+                v[a[k][j]]=1;
+                q.push(a[k][j]);
+            }
+        }
+       }
+       return count;
     }
 };
