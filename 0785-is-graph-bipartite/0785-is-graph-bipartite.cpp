d411578e3d3c4aebb84a47b7389d217023f3fe42
avinashtorm <avinashchandel223@gmail.com>
2026-09-21 23:27:58 +0530

    Time: 0 ms (100%), Space: 17.4 MB (59.17%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -5,8 +5,7 @@ bool dfs(int c,int cc,vector<int>&v,vector<vector<int>>&a){
     for(int i=0;i<a[c].size();i++){
         if(v[a[c][i]]==cc)return false;
         if(v[a[c][i]]!=0)continue;
-        if(cc==1)if(dfs(a[c][i],2,v,a)==false)return false;
-        if(cc==2)if(dfs(a[c][i],1,v,a)==false)return false;
+        if(dfs(a[c][i], 3 - cc, v, a) == false)return false;
     }
     return true;
 }
