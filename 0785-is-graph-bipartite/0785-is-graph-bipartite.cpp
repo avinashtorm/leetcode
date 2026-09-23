3cf4138c5c91dbf3a86d6f9c7859f1b5cf9cb0f3
avinashtorm <avinashchandel223@gmail.com>
2026-09-23 10:22:53 +0530

    Time: 0 ms (100%), Space: 17.5 MB (59.14%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-bool dfs(int c,int cc,vector<int>&v,vector<vector<int>>&a){
-    v[c]=cc;
-    for(int i=0;i<a[c].size();i++){
-        if(v[a[c][i]]==cc)return false;
-        if(v[a[c][i]]!=0)continue;
-        if(dfs(a[c][i], 3 - cc, v, a) == false)return false;
-    }
-    return true;
-}
-    bool isBipartite(vector<vector<int>>&a){
+    bool isBipartite(vector<vector<int>>&a) {
         int n=a.size();
         vector<int>v(n,0);
+        queue<int>q;
         for(int i=0;i<n;i++){
-                if(v[i]!=0)continue;
-                if(dfs(i,1,v,a)==false)return false;
+            if(v[i]!=0)continue;
+            q.push(i);
+            v[i]=1;
+            while(!q.empty()){
+                int x=q.front();
+                q.pop();
+                for(int j=0;j<a[x].size();j++){
+                    if(v[x]==v[a[x][j]])return false;
+                    if(v[a[x][j]]!=0)continue;
+                    q.push(a[x][j]);
+                    v[a[x][j]]=3-v[x];
+                }
+            }
         }
         return true;
     }
