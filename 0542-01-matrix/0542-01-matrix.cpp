997d6ebff70f7647cc79bb5902b09e3ebb6dca02
avinashtorm <avinashchandel223@gmail.com>
2026-09-23 22:42:19 +0530

    Time: 17 ms (60.78%), Space: 36.1 MB (56.94%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>&a){
+    vector<vector<int>> updateMatrix(vector<vector<int>>&a) {
         int n=a.size(),m=a[0].size();
-        vector<vector<int>>dist;
-        dist.assign(n,vector<int>(m,INT_MAX));
         queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(a[i][j]==0){
@@ -16,19 +15,18 @@ public:
         int dx[]={1,-1,0,0};
         int dy[]={0,0,-1,1};
         while(!q.empty()){
-            int k=q.size();
-            while(k--){
-            auto[g,h]=q.front();
-            int x=g.first,y=g.second;
+            auto[x,y]=q.front();
+            int x0=x.first,x1=x.second;
+            dist[x0][x1]=y;
             q.pop();
             for(int i=0;i<4;i++){
-                if(x+dx[i]<n&&x+dx[i]>=0&&y+dy[i]<m&&y+dy[i]>=0){
-                    if(dist[x+dx[i]][y+dy[i]]!=INT_MAX)continue;
-                    q.push({{x+dx[i],y+dy[i]},h+1});
-                    dist[x+dx[i]][y+dy[i]]=min(dist[x+dx[i]][y+dy[i]],h+1);
+                int nx=x0+dx[i];
+                int ny=x1+dy[i];
+                if(nx<n&&nx>=0&&ny<m&&ny>=0){
+                    if(dist[nx][ny]!=INT_MAX)continue;
+                    dist[nx][ny]=y+1;
+                    q.push({{nx,ny},y+1});
                 }
-            }
-
             }
         }
         return dist;
